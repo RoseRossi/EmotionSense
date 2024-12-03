b1f0c58d6c9ad45d1fe4f6c5057fea2ee10748d2
Paula Giraldo 2022411 <maria.paula.giraldo@correounivalle.edu.co>
2024-12-03 12:50:54 -0500

    landing wo bg

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,48 @@
 import React, { useEffect } from "react";
+import { Box } from '@mui/material';
+import { styled } from '@mui/system';
+
+
 import { testBackend } from "../services/api";
+import "./Home.css";
+import emotionlogo from "../assets/emotionlogo.svg";
+import bg from "../assets/bg.svg";
 
 const Home = () => {
   useEffect(() => {
     testBackend();
   }, []);
 
+const BackgroundImage = styled(Box)({
+  backgroundImage: 'url(/src/assets/bg.svg)', 
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  height: '100vh',
+  width: '100vw',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <>
+    <BackgroundImage>
       <h1>EmotionSense</h1>
-      <p>El frontend está funcionando correctamente</p>
-    </div>
+      <img src={emotionlogo} className="logo" alt="Logo emotion" />
+      <div className="cardLand">
+        <p>
+          Welcome to your personal emocional assistant. Click on the button down below to start your journal.
+        </p>
+        <button>
+          Start
+        </button>
+      </div>
+      <p className="under-text">
+        We see and we care.
+      </p>
+    </BackgroundImage>
+    </>
   );
 };
 
